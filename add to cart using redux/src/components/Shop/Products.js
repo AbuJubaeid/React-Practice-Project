094545a7331b1430426065a81057ae1a65f428d0
AbuJubaeid <abujubaeidpranto2@gmail.com>
2023-12-20 17:11:43 +0600

    Add files via upload

--- add to cart using redux/src/components/Shop/Products.js
+++ add to cart using redux/src/components/Shop/Products.js
@@ -1,1 +1,37 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
 
+const Products = (props) => {
+  const DUMMY_PRODUCT = [{
+    id:'p1',
+    price: 5,
+    title:'My First Book',
+    description:'The first book I ever wrote'
+  },
+  {
+    id:'p2',
+    price: 6,
+    title:'My Second Book',
+    description:'The Second book I ever wrote'
+  }
+]
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCT.map((product)=>(
+          <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />
+        ))
+        }
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
