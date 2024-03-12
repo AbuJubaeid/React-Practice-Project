44a7a96f9856f99210e683aa132be6e3d87c93e3
AbuJubaeid <abujubaeidpranto2@gmail.com>
2024-03-12 15:20:22 +0600

    Update productSaga.js

--- redux-saga/src/redux/productSaga.js
+++ redux-saga/src/redux/productSaga.js
@@ -1,15 +1,13 @@
-import { put, takeEvery } from 'redux-saga/effects';
-import { product_Data, set_product } from "./constant";
+import { put, takeEvery } from 'redux-saga/effects'
+import { productdata, setdata } from "./constant"
 
-function* getProductData(){
-    let data = yield fetch("http://localhost:3000/products");
+function* getProduct(){
+    let data = yield fetch('http://localhost:3000/products')
     data = yield data.json()
-    yield put({type:set_product, data})
-
+    yield put({type:setdata, data})
 }
 
 function* saga(){
-    yield takeEvery(product_Data, getProductData)
+    yield takeEvery(productdata, getProduct)
 }
-
 export default saga
