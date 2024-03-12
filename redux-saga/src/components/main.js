b128ea1e0a8675e07fb7b0049cf9eb6dfc8beb98
AbuJubaeid <abujubaeidpranto2@gmail.com>
2024-03-12 15:18:02 +0600

    Update main.js

--- redux-saga/src/components/main.js
+++ redux-saga/src/components/main.js
@@ -1,43 +1,53 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { addToCart, removeFromCart } from "../redux/action"
-import productData from "../redux/productAction"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AddToCart, RemoveFromCart } from "../redux/action";
+import { ProductData } from "../redux/productAction";
 
+function Main() {
+  let data = useSelector((state) => state.ProductReducer);
+  const dispatch = useDispatch();
+  // const Product = {
+  //   name: "I-Phone",
+  //   price: 10000,
+  //   category: "mobile",
+  // };
 
+  useEffect(() => {
+    dispatch(ProductData());
+  });
 
+  return (
+    <div className="main">
+      {/* <button className="empty" onClick={() => dispatch(EmptyCart(data))}>
+        Empty_Cart
+      </button> */}
 
+      <div className="main-content">
+        {data.map((item) => (
+          <div className="main-data" key={item.id}>
+            <img src={item.photo} alt="" />
+            <div className="main-text">
+              <div>Name: {item.name}</div>
+              <div>Price: {item.price}</div>
+              <div>Category: {item.category}</div>
+              <div>Brand: {item.brand}</div>
+            </div>
 
-
-const Main =()=>{
-    const data = useSelector((state)=>state.productReducer)
-    const dispatch = useDispatch()
-
-    useEffect(()=>{
-        dispatch(productData())
-      },[])
-      
-    return(
-    <div>
-        <div>
-            {
-                data.map((item)=> 
-                <div key={item.id}>
-                    <img src={item.photo} alt=""/>
-                    <div>Name: {item.name}</div>
-                    <div>Color: {item.color}</div>
-                    <div>Price: {item.price}</div>
-                    <div>Brand: {item.brand}</div>
-                    <div>
-                    <button onClick={()=>dispatch(addToCart(item))}>Add-To-Cart</button>
-                    </div>
-                    <button onClick={()=>dispatch(removeFromCart(item.id))}>Remove-From-Cart</button>
-                </div>
-                )
-            }  
-        </div>
-        
+            <div className="add-button">
+              <button  onClick={() => dispatch(AddToCart(item))}>
+                Add_To_Cart
+              </button>
+            </div>
+            <div>
+              <button onClick={() => dispatch(RemoveFromCart(item.id))}>
+                Remove_From_Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-        
-    )
+  );
 }
-export default Main
+
+export default Main;
