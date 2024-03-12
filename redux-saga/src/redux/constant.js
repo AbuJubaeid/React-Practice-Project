99a60cbd66ef791e91d9dbfcf7d7d90a66a89b13
AbuJubaeid <abujubaeidpranto2@gmail.com>
2024-03-12 15:19:07 +0600

    Update constant.js

--- redux-saga/src/redux/constant.js
+++ redux-saga/src/redux/constant.js
@@ -1,4 +1,5 @@
-export const add_to_cart = "add_to_cart";
-export const remove_from_cart = "remove_from_cart";
-export const product_Data = "product_Data"
-export const set_product = "set_product"
+export const addtocart ="ADD_T0_CART"
+export const removefromcart ="REMOVE_FROM_CART"
+export const emptycart ="EMPTY_CART"
+export const productdata ="PRODUCT_DATA"
+export const setdata = "SET_DATA"
